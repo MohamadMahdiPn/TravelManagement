6dc1bdc9cc4c715bca4763b06b85ef785fe3d5ad
محمد مهدی پورنجفی <m.p_996@hotmail.com>
2023-05-12 16:45:48 +0330

    AddWeatherService

---
+++ TravelManagement.Infrastructure/Services/WeatherService.cs
@@ -0,0 +1,11 @@
+﻿using TravelManagement.Application.DTO.External;
+using TravelManagement.Application.Services;
+using TravelManagement.Domain.ValueObjects;
+
+namespace TravelManagement.Infrastructure.Services;
+
+public class WeatherService:IWeatherService
+{
+    public Task<WeatherDto> GetWeatherAsync(TravelerCheckListDestination destination) =>
+        Task.FromResult(new WeatherDto(new Random().Next(5, 30)));
+}
