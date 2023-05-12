105f270ab8c68e0bf156f6ebca112aefca72f1d6
محمد مهدی پورنجفی <m.p_996@hotmail.com>
2023-05-12 17:45:59 +0330

    Config Api

---
+++ TravelManagement.Api/Controllers/TravelerCheckListController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TravelManagement.Application.Commands;
+using TravelManagement.Application.DTO;
+using TravelManagement.Application.Queries;
+using TravelManagement.Shared.Abstractions.Commands;
+using TravelManagement.Shared.Commands;
+
+namespace TravelManagement.Api.Controllers
+{
+    public class TravelerCheckListController : BaseController
+    {
+        #region Constructor
+
+        private readonly ICommandDispatcher _commandDispatcher;
+        private readonly IQueryDispatcher _queryDispatcher;
+
+        public TravelerCheckListController(ICommandDispatcher commandDispatcher, IQueryDispatcher queryDispatcher)
+        {
+            _commandDispatcher = commandDispatcher;
+            _queryDispatcher = queryDispatcher;
+        }
+
+        #endregion
+
+
+        #region Get
+
+        [HttpGet("{id:guid}")]
+        public async Task<ActionResult<TravelCheckListDto>> Get([FromRoute] GetTravelerCheckList query)
+        {
+            var result = await _queryDispatcher.QueryAsync(query);
+            return OkOrNotFound(result);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TravelCheckListDto>>> Get([FromQuery] SearchTravelerCheckList query)
+        {
+            var result = await _queryDispatcher.QueryAsync(query);
+            return OkOrNotFound(result);
+        }
+
+        #endregion
+
+        #region Post
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] CreateTravelerCheckListWithItems command)
+        {
+            await _commandDispatcher.DispatchAsync(command);
+            return CreatedAtAction(nameof(Get), new { id = command.Id }, null);
+        }
+
+
+        #endregion
+
+        #region Put
+
+        [HttpPut("{TravelerCheckListId}/items")]
+        public async Task<IActionResult> Put([FromBody] AddTravelerItem command)
+        {
+            await _commandDispatcher.DispatchAsync(command);
+            return Ok();
+        }
+
+        [HttpPut("{TravelerCheckListId:guid}/items/{name}/Take")]
+        public async Task<IActionResult> Put([FromBody] TakeItem command)
+        {
+            await _commandDispatcher.DispatchAsync(command);
+            return Ok();
+        }
+
+        #endregion
+
+        #region Delete
+
+        [HttpDelete("{TravelerCheckListId:guid}/items/{name}")]
+        public async Task<IActionResult> Delete([FromBody] RemoveTravelerItem command)
+        {
+            await _commandDispatcher.DispatchAsync(command);
+            return Ok();
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<IActionResult> Delete([FromBody] RemoveTravelerCheckList command)
+        {
+            await _commandDispatcher.DispatchAsync(command);
+            return Ok();
+        }
+
+        #endregion
+
+    }
+}
--- TravelManagement.Api/Program.cs
+++ TravelManagement.Api/Program.cs
@@ -1,15 +1,22 @@
+using TravelManagement.Application;
+using TravelManagement.Infrastructure;
+using TravelManagement.Shared;
+
 var builder = WebApplication.CreateBuilder(args);
+ConfigurationManager configuration = builder.Configuration;
 
-// Add services to the container.
-
+builder.Services.AddShared();
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(configuration);
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -17,7 +24,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseShared();
 app.UseAuthorization();
 
 app.MapControllers();
